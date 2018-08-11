d72637d2e1cf785909b9c3d68ff3b8daf73c8d0a
Anthony Buchholz <anthony.buchholz@gmail.com>
2018-08-11 09:44:34 -0500

    Added Prefix and Suffix to basic person forms

--- frontend/forms/get_person.py
+++ frontend/forms/get_person.py
@@ -5,9 +5,11 @@ from wtforms.validators import Length, Optional
 
 
 class GetBasicPersonForm(FlaskForm):
+    prefix = StringField('Prefix', validators=[Length(max=10)])
     first_name = StringField('First Name', validators=[Length(max=30)])
     middle_name = StringField('Middle Name', validators=[Length(max=30)])
     last_name = StringField('Last Name', validators=[Length(max=30)])
+    suffix = StringField('Suffix', validators=[Length(max=20)])
     address = StringField('Address', validators=[Length(max=70)])
     mailing_address = StringField('Mailing Address (if different)', validators=[Length(max=30)])
     birth_date = DateField('Birth date', validators=[Optional()])
