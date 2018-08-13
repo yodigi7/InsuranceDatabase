4f534c672802a983b2cb515c7d1c4cf983689e6b
Anthony Buchholz <anthony.buchholz@gmail.com>
2018-08-13 11:26:10 -0500

    Added create_tables to allow for easy setup

---
+++ create_tables.py
@@ -0,0 +1,8 @@
+from database.flask_db import db
+from database.Person_Work import PersonWork
+from database.Person import Person
+from database.Person_Notes import PersonNotes
+
+
+if __name__ == '__main__':
+    db.create_all()
